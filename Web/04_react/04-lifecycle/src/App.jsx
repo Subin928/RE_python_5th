913812d36e605d3b028e3758a74957307644d0e1
Subin928 <ddd701160@gmail.com>
2026-02-05 14:36:59 +0900

    App.jsx 수정

--- Web/04_react/04-lifecycle/src/App.jsx
+++ Web/04_react/04-lifecycle/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
 import LifeCycleFunc from './LifeCycleFunc';
 import PostList from './PostList';
+import { Practice1, Practice2 } from './PracticeLifeCycle';
 
 function App() {
   return (
     <div className="App">
       <LifeCycleFunc />
       <PostList />
+
+      {/* 실습 */}
+      <Practice1 />
+      <Practice2 />
     </div>
   );
 }
