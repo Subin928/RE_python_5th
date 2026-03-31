9b6730f016c30b78523a1fc026f59e052f4c7825
Subin928 <ddd701160@gmail.com>
2026-03-31 11:36:37 +0900

    DDL(CREATE,ALTER,DROP) 문제 실습

--- DB/DDL/create.sql
+++ DB/DDL/create.sql
@@ -1,10 +1,3 @@
--- DDL (Data Definition Language)
--- 데이터베이스, 테이블을 정의하는 언어
-
--- 1. CREATE
-CREATE DATABASE test_db;
-DROP DATABASE test_db;
-
 -- 인코딩 지정
 CREATE DATABASE test_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -35,6 +28,19 @@ CREATE TABLE customer (
 SHOW TABLES;
 DESC customer;
 
+CREATE TABLE orders (
+	order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cust_id CHAR(10) NOT NULL,
+    prod_name CHAR(6) NOT NULL,
+    price INT NOT NULL,
+    amount smallint NOT NULL,
+    
+    FOREIGN KEY (cust_id) REFERENCES my_shop.customer(cust_id)
+);
+
+SHOW TABLES;
+DESC orders;
+
 -- ALTER
 -- 1. 속성 추가
 ALTER TABLE user ADD age INT;
@@ -44,3 +50,46 @@ ALTER TABLE user MODIFY address VARCHAR(40);
 ALTER TABLE user RENAME COLUMN address TO user_address;
 -- 4. 컬럼 삭제
 ALTER TABLE user DROP age;
+
+-- DDL 실습
+
+-- 1. 실습용 데이터베이스 university_db 생성
+CREATE DATABASE university_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- 2. students 테이블
+CREATE TABLE students (
+  student_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE,
+  major VARCHAR(30),
+  advisor_id INT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 3. professors 테이블
+CREATE TABLE professors (
+  professor_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE,
+  department VARCHAR(30),
+  mentee_id INT,
+  joined_at DATE
+);
+
+-- 4. 다음 관계가 만족되어야 함
+-- 4.1. students.advisor_id는 professors.professor_id 참조 (교수 정보가 삭제되면 해당 학생의 advisor_id)는 NULL로 설정
+ALTER TABLE students
+ADD CONSTRAINT fk_students_advisor
+FOREIGN KEY (advisor_id)
+REFERENCES professors(professor_id)
+ON DELETE SET NULL;
+
+-- 4.2 professors.mentee_id는 students.student_id를 참조 (학생 정보가 삭제되면 해당 교수의 mentee_id도 NUll로 설정)
+ALTER TABLE professors
+ADD CONSTRAINT fk_professors_mentee
+FOREIGN KEY (mentee_id)
+REFERENCES students(student_id)
+ON DELETE SET NULL;
+
+DESC students;
+DESC professors
