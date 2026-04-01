1566ce9c985c7cf8e10f43adc3e0c875b7dcc07c
Subin928 <ddd701160@gmail.com>
2026-04-01 13:17:39 +0900

    feat: SQL SELECT 및 GROUP BY/HAVING 실습 쿼리 작성

--- SQL/02_DML.sql
+++ SQL/02_DML.sql
@@ -139,14 +139,22 @@ GROUP BY class_id
 HAVING AVG(age) >= 24
 ORDER BY avg_age DESC;
 
+-- 실습 UPDATE와 DELETE
 
+SELECT * FROM student;
+SELECT * FROM class;
 
+UPDATE student 
+SET age = 26 
+WHERE name = "정국";
 
+UPDATE student
+SET age = age + 1
+WHERE class_id = "CLS01"
+AND age < 25;
 
+DELETE FROM class
+WHERE room LIKE "G%";
 
-
-
-
-
-
-
+DELETE FROM student
+WHERE join_date < "2024-03-01";
